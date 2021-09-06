2b0695628afed442a7fd38f053fa5a6847038478
Steve Tolzmann <steven.tolzmann@gmail.com>
2021-09-06 11:04:04 -0500

    Major overhaul

--- src/WakeOnLan.cs
+++ src/WakeOnLan.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Text.RegularExpressions;
 using System.IO;
+using System.Net.NetworkInformation;
 
 namespace WOLtool
 {
     public class WakeOnLan : IDisposable
     {
-        private bool _disposed = false;
         private readonly Socket _sock;
         private static readonly IPEndPoint[] _endpoints = new IPEndPoint[]
         {
-            new IPEndPoint(IPAddress.Broadcast, 7), // Common WOL Port
-            new IPEndPoint(IPAddress.Broadcast, 9) // Common WOL Port
+            new IPEndPoint(IPAddress.Broadcast, 7), // echo
+            new IPEndPoint(IPAddress.Broadcast, 9) // discard
         };
+
         public WakeOnLan()
         {
             _sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
@@ -24,6 +24,22 @@ namespace WOLtool
         }
 
         public void Send(string macAddress)
+        {
+            try
+            {
+                var macParse = PhysicalAddress.Parse(macAddress); // Parse string value
+                byte[] magicPacket = BuildMagicPacket(macParse); // Get magic packet byte array based on MAC Address
+                foreach (var ep in _endpoints) // Broadcast to *all* WOL Endpoints
+                {
+                    _sock.SendTo(magicPacket, ep); // Broadcast magic packet
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new WakeOnLanException("ERROR broadcasting WakeOnLan Magic Packet.", ex);
+            }
+        }
+        public void Send(PhysicalAddress macAddress)
         {
             try
             {
@@ -39,31 +55,26 @@ namespace WOLtool
             }
         }
 
-        private static byte[] BuildMagicPacket(string macAddress)
+        private static byte[] BuildMagicPacket(PhysicalAddress macAddress)
         {
-            macAddress = Regex.Replace(macAddress, "[. : -]", "");
-            if (macAddress.Length != 12) throw new ArgumentException("Invalid MAC Address Length! Must be 12 hexadecimal characters.");
-            byte[] macBytes = new byte[6];
-            for (int i = 0; i < 6; i++)
+            byte[] macBytes = macAddress.GetAddressBytes(); // Convert MAC Address to array of bytes
+            using (var ms = new MemoryStream())
+            using (var bw = new BinaryWriter(ms))
             {
-                macBytes[i] = Convert.ToByte(macAddress.Substring(i * 2, 2), 16);
-            }
-            using (MemoryStream ms = new MemoryStream())
-            using (BinaryWriter bw = new BinaryWriter(ms))
-            {
-                for (int i = 0; i < 6; i++)
+                for (int i = 0; i < 6; i++) // 6 times 0xFF
                 {
                     bw.Write((byte)0xff);
                 }
-                for (int i = 0; i < 16; i++)
+                for (int i = 0; i < 16; i++) // 16 times MAC Address
                 {
                     bw.Write(macBytes);
                 }
-                return ms.ToArray();
+                return ms.ToArray(); // 102 Byte Magic Packet
             }
         }
 
         // Public implementation of Dispose pattern callable by consumers.
+        private bool _disposed = false;
         public void Dispose() => Dispose(true);
 
         // Protected implementation of Dispose pattern.
